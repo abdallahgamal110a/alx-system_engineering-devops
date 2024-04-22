efb6e0ed9f5e6b57ead405312ea3c27fe9468ee7
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-04-22 17:49:40 +0200

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,19 @@
 # puppet declarative script to install flask from pip3.
 
+package { 'python3':
+  ensure => 'present'
+}
+
+package { 'python3-pip':
+  ensure => 'present'
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+}
+
+package { 'werkzeug':
+  ensure => '2.1.1',
+  provider => 'pip',
 }
